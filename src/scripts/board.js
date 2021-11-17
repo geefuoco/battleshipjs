614d5ffbc2952ffb6cffab9d0fb582f2a47cf59f
Gianluca Fuoco <gianluca_1227@hotmail.com>
2021-11-17 17:14:06 -0500

    Added board and it's tests

---
+++ src/scripts/board.js
@@ -0,0 +1,135 @@
+import tileFactory from "./tile.js";
+
+const boardFactory = (dimension) => {
+  const boardSize = dimension;
+  const gameBoard = createBoard(boardSize);
+  const ships = [];
+  const sunkenShips = [];
+  const shipMap = {};
+
+  function createBoard(boardSize) {
+    const board = [];
+    for (let i = 0; i < boardSize; i++) {
+      board[i] = [];
+      for (let j = 0; j < boardSize; j++) {
+        board[i].push(tileFactory(i, j));
+      }
+    }
+    return board;
+  }
+
+  const getBoard = () => {
+    return [...gameBoard];
+  };
+
+  const getTile = (x, y) => {
+    if (outOfBounds(x, y)) {
+      throw "ERROR: Outside of board";
+    }
+    return gameBoard[x][y];
+  };
+
+  const outOfBounds = (x, y) => {
+    return x > boardSize || y > boardSize || x < 0 || y < 0;
+  };
+
+  const getSurroundingTiles = (x, y) => {
+    const surroundingTiles = {};
+    addValidTiles(x, y, surroundingTiles);
+    return surroundingTiles;
+  };
+
+  const addValidTiles = (x, y, object) => {
+    const values = [-1, 1];
+    for (let i of values) {
+      if (!outOfBounds(x + i, y)) {
+        object[[x, y]] = getTile(x + i, y);
+      }
+      if (!outOfBounds(x, y + i)) {
+        object[[x, y + i]] = getTile(x, y + i);
+      }
+      if (!outOfBounds(x + i, y + i)) {
+        object[[x + i, y + i]] = getTile(x + i, y + i);
+      }
+      if (!outOfBounds(x - i, y + i)) {
+        object[[x - i, y + i]] = getTile(x - i, y + i);
+      }
+      if (!outOfBounds(x + i, y - i)) {
+        object[[x + i, y - i]] = getTile(x + i, y - i);
+      }
+    }
+  };
+
+  const markHit = (x, y) => {
+    const tile = getTile(x, y);
+    tile.setState(tile.HIT);
+    if (tile.getShip()) {
+      tile.getShip().hit();
+      updateBoard();
+    }
+  };
+
+  const updateBoard = () => {
+    const sunkShips = ships.filter((ship) => ship.isSunk() === true);
+    for (let ship of sunkShips) {
+      if (sunkenShips.includes(ship)) continue;
+      filterShipTiles(ship);
+      sunkenShips.push(ship);
+    }
+  };
+
+  const filterShipTiles = (ship) => {
+    const tiles = shipMap[ship];
+    tiles.forEach((tile) => {
+      const position = tile.getPosition();
+      markTilesHit(position);
+    });
+  };
+
+  const markTilesHit = (position) => {
+    const surroundingTiles = getSurroundingTiles(position[0], position[1]);
+    for (let [key, value] of Object.entries(surroundingTiles)) {
+      value.setState(value.HIT);
+    }
+  };
+
+  const placeShip = (ship, coords) => {
+    const tile = getTile(coords[0], coords[1]);
+    const length = ship.getLength();
+    const shipTiles = [];
+    if (tile.getState() === tile.OCCUPIED) {
+      throw "ERROR: Ship already on tile";
+    }
+    for (let i = 0; i < length; i++) {
+      let nextTile = null;
+      if (ship.HORIZONTAL) {
+        nextTile = getTile(coords[0], coords[1] + i);
+      } else {
+        nextTile = getTile(coords[0] + i, coords[1]);
+      }
+      nextTile.setShip(ship);
+      shipTiles.push(nextTile);
+    }
+    tile.setShip(ship);
+    shipTiles.push(tile);
+    ships.push(ship);
+    shipMap[ship] = shipTiles;
+  };
+
+  const allSunk = () => {
+    return (
+      ships.length === ships.filter((ship) => ship.isSunk() === true).length
+    );
+  };
+
+  return {
+    getBoard,
+    getTile,
+    getSurroundingTiles,
+    markHit,
+    placeShip,
+    allSunk,
+  };
+};
+
+export default boardFactory;
---
+++ test/board.test.js
@@ -0,0 +1,152 @@
+import boardFactory from "../src/scripts/board.js";
+
+const board = boardFactory(10);
+const ship = {
+  HORIZONTAL: 1,
+  getLength: function () {
+    return 2;
+  },
+  getOrientation: function () {
+    return ship.HORIZONTAL;
+  },
+  hit: function () {},
+  isSunk: function () {
+    return true;
+  },
+};
+
+describe("when getting a tile", () => {
+  test("it should return the tile from the position given", () => {
+    expect(typeof board.getTile(1, 2)).toBe("object");
+  });
+
+  test("it should throw error when getting a tile out of bounds", () => {
+    expect(() => board.getTile(50, 30)).toThrow();
+  });
+});
+
+describe("when it gets the surrounding tiles", () => {
+  test("it should get an array of surrounding tiles", () => {
+    expect(typeof board.getSurroundingTiles(1, 2)).toBe("object");
+  });
+
+  test("it should only get 3 tiles if its a corner", () => {
+    const tiles = Object.entries(board.getSurroundingTiles(0, 0));
+    let count = 0;
+    for (let i of tiles) {
+      count++;
+    }
+    expect(count).toBe(3);
+  });
+});
+
+describe("when marking a hit", () => {
+  const tile = board.getTile(2, 2);
+  tile.setShip(ship);
+  const hit = jest.spyOn(tile, "setState");
+  const testShip = jest.spyOn(ship, "hit");
+  beforeEach(() => {
+    board.markHit(2, 2);
+  });
+  test("it should mark the tile as hit", () => {
+    expect(hit).toHaveBeenCalled();
+    hit.mockRestore();
+  });
+
+  test("the ship should take damage if it is hit", () => {
+    expect(testShip).toHaveBeenCalled();
+    testShip.mockRestore();
+  });
+});
+
+describe("when placing a ship on the board", () => {
+  test("it should update the tile to have a ship", () => {
+    const tile = board.getTile(4, 4);
+    const spy = jest.spyOn(tile, "setShip");
+    board.placeShip(ship, [4, 4]);
+    expect(spy).toHaveBeenCalled();
+  });
+
+  test("it should throw an error if a ship is already present", () => {
+    const otherShip = {};
+    expect(() => board.placeShip(otherShip, [4, 4])).toThrow();
+  });
+
+  test("it should cover the correct tiles", () => {
+    const otherTile = board.getTile(4, 5);
+    expect(otherTile.getShip()).toBe(otherTile.getShip());
+  });
+});
+
+describe("when all the ships have been sunk", () => {
+  const newBoard = boardFactory(10);
+  const testShip = {
+    HORIZONTAL: 1,
+    getLength: function () {
+      return 2;
+    },
+    getOrientation: function () {
+      return ship.HORIZONTAL;
+    },
+    hit: function () {},
+    isSunk: function () {
+      return true;
+    },
+  };
+  const testShip2 = {
+    HORIZONTAL: 1,
+    getLength: function () {
+      return 2;
+    },
+    getOrientation: function () {
+      return ship.HORIZONTAL;
+    },
+    hit: function () {},
+    isSunk: function () {
+      return true;
+    },
+  };
+
+  newBoard.placeShip(testShip, [0, 0]);
+  newBoard.placeShip(testShip2, [5, 5]);
+
+  test("should send a message is isSunk method of ship", () => {
+    const spy = jest.spyOn(testShip, "isSunk");
+    newBoard.allSunk();
+    expect(spy).toHaveBeenCalled();
+    spy.mockRestore();
+  });
+
+  test("it should return true", () => {
+    expect(newBoard.allSunk()).toBeTruthy();
+  });
+});
+
+describe("when a ship has been sunk", () => {
+  const board = boardFactory(10);
+  const ship = {
+    HORIZONTAL: 1,
+    getLength: function () {
+      return 2;
+    },
+    getOrientation: function () {
+      return ship.HORIZONTAL;
+    },
+    hit: function () {},
+    isSunk: function () {
+      return true;
+    },
+  };
+  board.placeShip(ship, [3, 3]);
+  board.markHit(3, 3);
+  test("it should mark all the tiles around it as hit", () => {
+    const tiles = Object.entries(board.getSurroundingTiles(3, 3));
+    let marked = true;
+    for (let [key, tile] of tiles) {
+      if (tile.getState() === tile.EMPTY) {
+        marked = false;
+      }
+    }
+    expect(marked).toBeTruthy();
+  });
+});

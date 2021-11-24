6af3eccf273c51347601b35d86a137087e7ae3bd
Gianluca Fuoco <gianluca_1227@hotmail.com>
2021-11-24 16:11:45 -0500

    Added a rotate button component for the rotate button element

---
+++ src/components/rotateButton.js
@@ -0,0 +1,9 @@
+const rotate = () => {
+  const button = document.createElement("button");
+  button.classList.add("rotate");
+  button.innerText = "Rotate Ships";
+
+  return button;
+};
+
+export default rotate;
